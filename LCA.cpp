635c64fb86e04c34598f261bae56ebf6650c6528
walshk22 <43147647+walshk22@users.noreply.github.com>
2018-09-24 22:58:03 +0100

    Add files via upload
    Fixed error for Nodes that aren't on the tree. Added new functions to check for nodes and print function

--- LCA.cpp
+++ LCA.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 vector <int> createTree()
 {
-    vector<int> NodeTree = {0,1,2,3,4,5,6,7,8,9,10,11};
+    vector<int> NodeTree = {1,2,3,4,5,6,7,8,9,10,11, 12};
     
     return NodeTree;
 }
@@ -22,6 +22,30 @@ vector <int> createTree()
 int findLCA(vector<int> Tree, Node a, Node b)
 {
     int LCA;
+    bool AonTree = false;
+    bool BonTree = false;
+    
+    
+    //EDITTED-- Checking for validity of nodes being checked
+    for(int i = 0; i<Tree.size(); i++)
+    {
+        if(a.getNum() == Tree[i])
+        {
+            AonTree = true;
+        }
+        
+        if(b.getNum() == Tree[i])
+        {
+            BonTree = true;
+        }
+    }
+    
+    if(BonTree== false || AonTree == false)
+    {
+        cout << "ERROR: THE NODE REQUESTED IS NOT ON THE TREE" << endl;
+        return 0;
+    }
+    
     if(a.getNum()==b.getNum())
     {
         LCA= a.getNum();
@@ -59,6 +83,32 @@ int findLCA(vector<int> Tree, Node a, Node b)
     return LCA;
 }
 
+//Check LCA to see if it equals 0; if so the LCA has an error and nothing should be outprinted
+bool checkLCA(int LCA)
+{
+    if(LCA == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+void printLCA(int LCA, bool check)
+{
+    if(check)
+    {
+        cout <<  "The LCA between the two nodes is " << LCA << endl;
+    }
+    
+    else
+    {
+        cout << "LCA is N/A" << endl;
+    }
+}
+
 int main()
 {
 
@@ -82,14 +132,29 @@ int main()
     }
     
     int LCA = findLCA(Tree, a, b);
-    
-    cout << "The LCA of " << a.getNum() << " and " << b.getNum() << " is " << LCA << endl;
+    bool checkingLCA = checkLCA(LCA);
+    cout << "NODE A = " << a.getNum() << endl;
+    cout << "NODE B = " << b.getNum() << endl;
+    printLCA(LCA, checkingLCA);
     
     Node c = Node(10);
     Node d = Node(11);
     
     int LCA1 = findLCA(Tree, c, d);
-    cout << "LCA of "<< c.getNum() << " and " << d.getNum() << " is " << LCA1 << endl;
+    bool checkingLCA1 = checkLCA(LCA1);
+    cout << "NODE C = " << c.getNum() << endl;
+    cout << "NODE D = " << d.getNum() << endl;
+    printLCA(LCA1, checkingLCA1);
+    
+    Node e = Node(14);
+    Node f= Node(4);
+    
+    //EDIT ME: LCA is correct but there should be an error because 14 is not on the NODE Tree
+    int LCA2 = findLCA(Tree, e, f);
+    bool checkingLCA2 = checkLCA(LCA2);
+    cout << "NODE E = " << e.getNum() << endl;
+    cout << "NODE F = " << f.getNum() << endl;
+    printLCA(LCA2, checkingLCA2);
     
     return 0;
 }
