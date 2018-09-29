d82fc7507e1998eb3590edc16055866d0bdc9895
walshk22 <43147647+walshk22@users.noreply.github.com>
2018-09-29 17:21:53 +0100

    Add files via upload
    Double branched Node Tree-- increasing complexity of tree

---
+++ main.cpp
@@ -0,0 +1,100 @@
+//
+//  Double_Branch_LCA.cpp
+//  Lowest Common Ancestor
+//
+//  Created by Kaitlyn Walsh on 9/29/18.
+//  Copyright © 2018 Kaitlyn Walsh. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> nodeTree = { 1, 2, 4, 2, 5, 2, 1, 3, 6, 3, 7, 3, 1};
+vector<int> treeDepth = {0, 1, 2, 1, 2, 1, 0, 1, 2, 1, 2, 1, 0};
+
+int findLCA ( int a, int b);
+
+int main ()
+{
+    
+    int LCA = findLCA( 4,5);                    //LCA = 2
+    cout << "THE LCA(4,5) IS: " << LCA << endl;
+    
+    int LCA1 = findLCA(2,3);                        //LCA = 1
+    cout << "THE LCA(2,3) IS: " << LCA1 << endl;
+    
+    int LCA2 = findLCA(6,7);                        //LCA =3
+    cout << "THE LCA(6,7) IS: " << LCA2 << endl;
+    
+    
+    return 0;
+}
+
+
+int findLCA( int a, int b)
+{
+    int aDepth, bDepth, LCA=0;
+    
+    //Finding depth of the
+    for(int i =0; i<13; i++)
+    {
+        if(nodeTree[i] == a)
+        {
+            aDepth = treeDepth[i];
+        }
+        
+        if(nodeTree[i] == b)
+        {
+            bDepth = treeDepth[i];
+        }
+    }
+    
+    if(aDepth == 1 && bDepth ==1)
+    {
+        LCA = 1;
+    }
+    
+    else if((aDepth == 1 && bDepth == 2) || (bDepth ==1 && aDepth == 2) )
+    {
+        LCA = 1;
+    }
+    
+    else{
+        int aPos, bPos;                               // place holders for nodes a and b
+        
+        for(int i =0; i< 13; i++)
+        {
+            if(nodeTree[i] == a)
+            {
+                aPos = i;
+            }
+            
+            if(nodeTree[i]== b)
+            {
+                bPos = i;
+            }
+        }
+            
+            int LCAPosition=0;
+            if( aPos >bPos)
+            {
+                LCAPosition = (aPos-bPos/2);
+                LCA = nodeTree[LCAPosition];
+            }
+            else if(bPos>aPos)
+            {
+                LCAPosition = (bPos-aPos)/2;
+                LCA = nodeTree[LCAPosition];
+            }
+            
+            else{
+                LCA = 0;
+            }
+            
+        
+    }
+    
+    return LCA;
+}
+
